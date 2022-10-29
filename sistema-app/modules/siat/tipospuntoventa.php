abeb421532490b6a355b08bba276a0de86e64c6f
Etyvaldir607 <etyvaldirmc@gmail.com>
2022-10-29 13:52:03 -0400

    adición - debug puntos venta

--- sistema-app/modules/siat/tipospuntoventa.php
+++
@@ -1,9 +0,0 @@
-<?php
-ini_set('display_errors', 1);error_reporting(E_ALL);
-require_once __DIR__ . '/siat.php';
-
-//print_r($_GET);
-$items = siat_tipos_punto_venta(0, 0);
-
-header('Content-type: application/json');
-die(json_encode(['data' => $items]));
